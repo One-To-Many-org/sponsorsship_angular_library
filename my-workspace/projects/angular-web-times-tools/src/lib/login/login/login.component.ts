f887f3d92f85333e26656a5e0e0f03a6c78ebe68
Aremou <faizouaremou@gmail.com>
2021-02-27 16:13:55 +0100

    update library

--- my-workspace/projects/angular-web-times-tools/src/lib/login/login/login.component.ts
+++ my-workspace/projects/angular-web-times-tools/src/lib/login/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MediaButInfType } from '../types/media-but-info-type';
 
 @Component({
@@ -10,8 +11,21 @@ export class LoginComponent implements OnInit {
   @Input() mediaButInfTypes: MediaButInfType[];
   mediaButInfDatas: any[] = [];
   @Input() usernameField: string; // must receiving UsernameField[0] or UsernameField[1] type enum
+  @Output() loginValue = new EventEmitter<any>();
+  mobile = true;
 
-  constructor() { }
+  /**
+   * reative form
+   */
+  loginForm = this.fb.group(
+    {
+      username: ['', Validators.required], // control sur input username 
+      password: ['', Validators.required] // control sur input password
+    }
+  )
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
@@ -21,21 +35,53 @@ export class LoginComponent implements OnInit {
         case MediaButInfType.facebook:
           mediasButInf.icon = 'bootstrap-icons.svg#facebook';
           mediasButInf.name = MediaButInfType[0];
-          mediasButInf.color = 'btn-primary';
+
           break;
         case MediaButInfType.google:
           mediasButInf.icon = 'bootstrap-icons.svg#google';
           mediasButInf.name = MediaButInfType[1];
-          mediasButInf.color = 'btn-danger';
+
           break;
         default:
           mediasButInf.icon = 'bootstrap-icons.svg#linkedin';
           mediasButInf.name = MediaButInfType[2];
-          mediasButInf.color = 'btn-info';
+
       }
       this.mediaButInfDatas.push(mediasButInf);
     })
-    console.log(this.mediaButInfDatas);
+
+    const mediaQueryList = window.matchMedia('(min-width: 768px)');
+    mediaQueryList.addEventListener('change', this.screenTest);
   }
 
+
+  screenTest(event: any) {
+    if (event.matches) {
+      this.mobile = false;
+      console.log('it is not mobile', this.mobile);
+    } else {
+      this.mobile = true;
+      console.log('it is mobile', this.mobile);
+    }
+  }
+
+  buttonClass(isFirst: boolean, info: any): any {
+    return {
+      'btn-primary': info.name === MediaButInfType[0],
+      'btn-danger': info.name === MediaButInfType[1],
+      'btn-info': info.name === MediaButInfType[2],
+    }
+  }
+
+  /**
+   * sends his parent's login information via an output event
+   */
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.loginValue.emit(this.loginForm.value);
+    } else {
+      this.loginForm.markAsDirty();
+    };
+  }
 }
+
